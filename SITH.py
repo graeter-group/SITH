815768234c8ff43b6f0beb006ae201d0c7950ea4
mmfarrugia <mikaelamariefarrugia@gmail.com>
2022-07-26 12:08:16 +0200

    Juli 26 pre-lunch commit, docn refinement started

--- SITH.py
+++ SITH.py
@@ -23,8 +23,6 @@ class SITH:
 
         self.validateFiles()
 
-        # self.extractData()
-
     def extractData(self):
 
         self.getContents()
@@ -39,74 +37,99 @@ class SITH:
             dExtractor.extract()
             self.deformed.append(dExtractor.getGeometry())
 
-        # might get rid of if decide for the geometries to hold their respective hessians only, but this seems best for now
-        self.hMat = self.relaxed.hessian
+        # Defaults to the relaxed geometry Hessian, it is recommended to make new SITH objects for each new analysis for the
+        # sake of clearer output files but implementation of SITH.SetRelaxed() as a public function would enable the user to 
+        # manually swap the relaxed geometry with that of another geometry in the deformd list and then re-run analysis.
+        self.hessian = self.relaxed.hessian
 
         if self._kill:
-            self.kill()
+            self.__kill()
 
         self.validateGeometries()
 
         self.populateQ()
 
-    def setKillAtoms(self, atoms: list):
-        self._killAtoms = atoms
-        self._kill = True
+# region Atomic Homicide
 
-    def setKillDOFs(self, dofs: list):
-        self._killDOFs = dofs
-        self._kill = True
+    def __kill(self):
+        """Executes the removal of degrees of freedom (DOFs) specified and any associated with specified atoms."""
+        self.__killDOFs(self._killDOFs)
+        self.__killAtoms(self._killAtoms)
 
-    def kill(self):
-        self.killDOFs(self._killDOFs)
-        self.killAtoms(self._killAtoms)
+        """Use Case Note:
+        This is a private method to limit user error. Specification of these DOFs is made by the user programmatically with the public functions SetKillAtoms(atoms: list)
+        and SetKillDOFs(dofs: list) prior to data extraction by calling SITH.extractData(). If no mismatch between
+        number of DOFs in each geometry's coordinates and Hessian, this can be manually called after extractData()
+        as well but is not recommended."""
 
-    def killAtoms(self, atoms: list):
+    def __killAtoms(self, atoms: list):
         """
         Removes the indicated atoms from the JEDI analysis, as such it removes any associated degrees of freedom from
         the geometries' RICs as well as from the Hessian matrix.
         """
         for atomIndex in atoms:
-            self.killAtom(atomIndex)
+            self.__killAtom(atomIndex)
 
-    def killAtom(self, atom: int):
+    def __killAtom(self, atom: int):
         """
         Removes the indicated atoms from the JEDI analysis, as such it removes any associated degrees of freedom from
         the geometries' RICs as well as from the Hessian matrix.
         """
         dimsToKill = [dim for dim in self.relaxed.dimIndices if atom in dim]
-        dimIndicesToKill = [index for index in range(
-            self.relaxed.dims[0]) if atom in self.relaxed.dimIndices[index]]
-        self.killDOFs(dimsToKill)
+        self.__killDOFs(dimsToKill)
 
-
-    def killDOFs(self, dofs: list[Tuple]):
+    def __killDOFs(self, dofs: list[Tuple]):
         """
         Removes the indicated degrees of freedom from the JEDI analysis, as such it removes them from the geometries' RICs
         as well as from the Hessian matrix.
         """
-        # blah = list()
-        # for dof in dofs:
-        #     for i in range(self.relaxed.dims[0]):
-        #         if self.relaxed.dimIndices[i] == dof:
-        #             blah.append(i)
         rIndices = list()
         dIndices = list()
         for dof in dofs:
-            rIndices.extend([i for i in range(self.relaxed.dims[0]) if self.relaxed.dimIndices[i] == dof])
-            dIndices.extend([i for i in range(self.deformed[0].dims[0]) if self.deformed[0].dimIndices[i] == dof])
+            rIndices.extend([i for i in range(self.relaxed.dims[0])
+                            if self.relaxed.dimIndices[i] == dof])
+            dIndices.extend([i for i in range(self.deformed[0].dims[0])
+                            if self.deformed[0].dimIndices[i] == dof])
         self.relaxed.killDOFs(rIndices)
         for deformation in self.deformed:
             deformation.killDOFs(dIndices)
 
-    # def killDOFs(self, dofs: list[Tuple]):
-    #     """
-    #     Removes the indicated degrees of freedom from the JEDI analysis, as such it removes them from the geometries' RICs
-    #     as well as from the Hessian matrix.
-    #     """
+# endregion
 
-    #     indices = [[indexOf(self.relaxed.dimIndices[i], dof) for i in range(self.relaxed.dims[0])] for dof in dofs]
-    #     self.killDOFs(indices)
+# region Public Functions meant to be used by novice-intermediate user
+
+    def setKillAtoms(self, atoms: list):
+        """
+        Sets the atoms which should be removed from SITH analysis, must be used prior to calling SITH.extractData().
+        """
+        self._killAtoms = atoms
+        self._kill = True
+
+    def setKillDOFs(self, dofs: list):
+        """
+        Sets the DOFs (degrees of freedom) which should be removed from SITH analysis, must be used prior to calling SITH.extractData().
+        """
+        self._killDOFs = dofs
+        self._kill = True
+
+    def setRelaxed(self, geometryName: str):
+        """
+        Replaces the current relaxed geometry used for calculation of stress energy with the specified geometry, pushing the current
+        relaxed geometry to the deforation list.
+        If the specified geometry does not exist or has no Hessian, the relaxed geometry will simply not be changed. Deformation
+        energy is still calculated for all geometries aside from the new relaxed geometry.
+        """
+
+        """Implementation Instructions:
+        TODO"""
+        raise NotImplementedError("Unimplemented due to current lack of necessity, contact @mmfarrugia on github for more info.")
+        """Use Case Note:
+        This can be useful for cases where the error increases unacceptably and multiple reference points along a 
+        deformation are needed in order to have a more accurate energy calculation as the stretching coordinate progresses."""
+
+# endregion
+
+# region Validation
 
     def validateFiles(self):
         """
@@ -116,6 +139,15 @@ class SITH:
         assert self.defPath.exists(), "Path to deformed geometry data does not exist."
 
         self.dDir = self.defPath.is_dir()
+
+    def validateGeometries(self):
+        """
+        Ensure that the relaxed and deformed geometries are compatible(# atoms, # dofs, etc.)
+        """
+        assert all([deformn.numAtoms() == self.relaxed.numAtoms() and all([deformn.dims[i] == self.relaxed.dims[i] for i in range(
+            4)]) for deformn in self.deformed]), "Incompatible number of atoms or dimensions amongst input files."
+
+# endregion
 
     def getContents(self):
         """
@@ -147,13 +179,6 @@ class SITH:
                 "An exception occurred during the extraction of the input files' contents.")
             sys.exit(sys.exc_info()[0])
 
-    def validateGeometries(self):
-        """
-        Ensure that the relaxed and deformed geometries are compatible(# atoms, # dofs, etc.)
-        """
-        assert all([deformn.numAtoms() == self.relaxed.numAtoms() and all([deformn.dims[i] == self.relaxed.dims[i] for i in range(
-            4)]) for deformn in self.deformed]), "Incompatible number of atoms or dimensions amongst input files."
-
     def populateQ(self):
         self.q0 = np.zeros((self.relaxed.dims[0], 1))
         self.qF = np.zeros((self.relaxed.dims[0], 1))
@@ -178,7 +203,7 @@ class SITH:
                 dq[...] = np.abs(dq - 2*np.pi) if dq > (2*np.pi -
                                                         0.005) else (dq + 2*np.pi if dq < -(2*np.pi - 0.005) else dq)
 
-# TODO: reove the necessity of separately handling single deformation vs deformation vector
+# TODO: remove the necessity of separately handling single deformation vs deformation vector
     def energyAnalysis(self):
         self.energies = np.zeros((self.relaxed.dims[0], len(self.deformed)))
         self.deformationEnergy = np.zeros((1, len(self.deformed)))
