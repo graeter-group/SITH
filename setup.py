37ff67da32eaac22849fd7d3a16caac5fa572b9a
mmfarrugia <mikaelamariefarrugia@gmail.com>
2022-08-03 11:25:11 +0200

    retry with setup.cfg

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='sith',
-    version='0.0.5',
-    packages=find_packages(include=['SITH', 'SITH.*', 'Utilities.*', 'SithWriter.*']),
-    install_requires=[
-        'numpy>=1.13.3',
-        'ase>=3.22.1',
-        'setuptools'
-    ]
-)
+setup()
