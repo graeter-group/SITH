4fefcb079162f62be922c901af95ef1a88cf9aea
Mikaela Farrugia <mfarrugi@nd.edu>
2023-09-26 14:07:47 +0200

    Update conf.py remove project name
    check to see if doesn't nest files and can then identify the index.html file better

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'SITH'
+#project = 'SITH'
 copyright = '2022, Mikaela Farrugia, Daniel Sucerquia'
 author = 'Mikaela Farrugia, Daniel Sucerquia'
 release = '0.1.1'
