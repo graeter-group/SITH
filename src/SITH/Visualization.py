3489512fb393c8ef6e012fead4ded159f784b399
Sucerquia <dsucerg@gmail.com>
2023-10-10 19:22:54 +0200

    ADD: alignment of the molecule for visualization

--- src/SITH/Visualization.py
+++ src/SITH/Visualization.py
@@ -478,6 +478,89 @@ class MoleculeViewer:
     def picked(self):
         return self.viewer.view.picked
 
+    # Alignment
+
+    def rot_x(self, angle):
+        c = np.cos(angle)
+        s = np.sin(angle)
+        R = np.array([[1, 0, 0],
+                      [0, c, -s],
+                      [0, s, c]])
+        return R
+
+    def rot_y(self, angle):
+        c = np.cos(angle)
+        s = np.sin(angle)
+        R = np.array([[c, 0, s],
+                      [0, 1, 0],
+                      [-s, 0, c]])
+        return R
+
+    def rot_z(self, angle):
+        c = np.cos(angle)
+        s = np.sin(angle)
+        R = np.array([[c, -s, 0],
+                      [s, c, 0],
+                      [0, 0, 1]])
+        return R
+
+    def align_axis(self, vector):
+        """
+        Apply the necessary rotations to set a
+        vector aligned with positive x axis
+        """
+        xyproj = vector.copy()
+        xyproj[2] = 0
+        phi = np.arcsin(vector[2]/np.linalg.norm(vector))
+        theta = np.arccos(vector[0]/np.linalg.norm(xyproj))
+        if vector[1] < 0:
+            theta *= -1
+        trans = np.dot(self.rot_y(phi), self.rot_z(-theta))
+        return trans
+
+    def align_plane(self, vector):
+        """
+        Rotation around x axis to set a vector in the xy plane
+        """
+        reference = vector.copy()
+        reference[0] = 0
+        angle = np.arccos(reference[1]/np.linalg.norm(reference))
+        if reference[2] < 0:
+            angle *= -1
+        return self.rot_x(-angle)
+
+    def apply_trans(self, atoms, trans):
+        """
+        Apply a transformation to all vector positions of the
+        atoms object
+        """
+        new_positions = [np.dot(trans, atom.position) for atom in atoms]
+        atoms.set_positions(new_positions)
+        return new_positions
+
+    def xy_alignment(self, atoms, index1, index2, index3):
+        """
+        transforme the positions of the atoms such that
+        the atoms of indexes 1 and 2 are aligned in the
+        x axis
+
+        the atom 3 is in the xy plane"""
+        # center
+        center = (atoms[index1].position+atoms[index2].position)/2
+        atoms.set_positions(atoms.positions - center)
+        axis = atoms[index2].position
+        self.apply_trans(atoms, self.align_axis(axis))
+        third = atoms[index3].position
+        self.apply_trans(atoms, self.align_plane(third))
+
+        return atoms.positions
+
+    def show(self):
+        """
+        Show the molecule.
+        """
+        return self.box
+
 
 class VisualizeEnergies(MoleculeViewer):
     def __init__(self, sith_info, idef=0, alignment=None, axis=False,
